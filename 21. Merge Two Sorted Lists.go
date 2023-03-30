78881d50ff134e8e1e14941fea51c39a437758fe
febryanmz <febryan.zainal@gmail.com>
2023-03-30 20:46:56 +0700

    leetcode

--- 21. Merge Two Sorted Lists.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-
-}
--- 28. Find the Index of the First Occurrence in a String.go
+++ 28. Find the Index of the First Occurrence in a String.go
@@ -10,6 +10,7 @@ func main() {
 	needle := "sad"
 
 	fmt.Println(haystack, needle)
+	fmt.Println(strStr(haystack, needle))
 
 	//Output: 0
 	//Explanation: "sad" occurs at index 0 and 6.
@@ -23,17 +24,32 @@ func main() {
 	needle2 := "leeto"
 
 	fmt.Println(haystack2, needle2)
+	fmt.Println(strStr(haystack2, needle2))
 
 	//Output: -1
 	//Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
+	haystack3 := "dimanaakudimana"
+	needle3 := "aku"
+	fmt.Println(haystack3, needle3)
+	fmt.Println(strStr(haystack3, needle3))
+
 }
 
 func strStr(haystack string, needle string) int {
-	if len(haystack) < len(needle) {
-		return len(needle) - len(haystack)
+	n := len(haystack)
+	m := len(needle)
+
+	if m == 0 {
+		return 0
 	}
-	for i := 0; i < len(haystack); i++ {
-		if
+
+	for i := 0; i <= n-m; i++ {
+		fmt.Println("i : ", i, "haystack : ", haystack[i:i+m])
+		if haystack[i:i+m] == needle {
+			return i
+		}
 	}
+
+	return -1
 }
