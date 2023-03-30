5b97ebaff0f3f15afcd5c1bf31f1395cf75cbee8
febryanmz <febryan.zainal@gmail.com>
2023-03-30 09:36:15 +0700

    leetcode

--- 118. Pascal's Triangle.go
+++ 118. Pascal's Triangle.go
@@ -1,1 +1,26 @@
-package leetcode
+package main
+
+import (
+	"fmt"
+)
+
+func generate(numRows int) [][]int {
+	var res = make([][]int, numRows)
+
+	for i := 0; i < numRows; i++ {
+		res[i] = make([]int, i+1)
+		res[i][0], res[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j] + res[i-1][j-1]
+		}
+	}
+
+	return res
+
+}
+
+func main() {
+	fmt.Println(generate(1))
+	fmt.Println(generate(5))
+	fmt.Println(generate(10))
+}
